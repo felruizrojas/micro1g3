50a02f956e4448541ecb086c07ecce8334216aee
fel <fe.ruizr@duocuc.cl>
2025-05-25 22:02:47 -0400

    get&postCasiListos

--- src/main/java/com/micro1/micro1g3/service/RolService.java
+++ src/main/java/com/micro1/micro1g3/service/RolService.java
@@ -1,11 +1,14 @@
 package com.micro1.micro1g3.service;
 
+import com.micro1.micro1g3.model.Permiso;
 import com.micro1.micro1g3.model.Rol;
 import com.micro1.micro1g3.model.Usuario;
+import com.micro1.micro1g3.repository.PermisoRepository;
 import com.micro1.micro1g3.repository.RolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +19,9 @@ public class RolService {
     @Autowired
     private RolRepository rolRepository;
 
+    @Autowired
+    private PermisoRepository permisoRepository;
+
     public List<Rol> listarRoles() {
         return rolRepository.findAll();
     }
@@ -25,6 +31,12 @@ public class RolService {
     }
 
     public Rol crearRol(Rol rol) {
+        List<Permiso> permisosGuardados = new ArrayList<>();
+        for (Permiso permiso : rol.getPermisos()) {
+            Permiso guardado = permisoRepository.save(permiso); // Guardar primero
+            permisosGuardados.add(guardado);
+        }
+        rol.setPermisos(permisosGuardados);
         return rolRepository.save(rol);
     }
 }
